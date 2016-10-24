24e6905d7d7b92820f7dfc9bc3a3cc2eda00826e
hbsis-marcosbusana <marcos.busana@hbsis.com.br>
2016-10-24 09:55:09 -0200

    Configurações

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 
 module.exports  = {
   servidores : [
-    {nome: 'Guarulhos', url: 'localhost:8001', servicos : ['blitz-refugo']},
-    {nome: 'SP', url: '54.94.190.76:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
-    {nome: 'MG-NO', url: '54.94.190.76:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
-    {nome: 'Sul-RJ', url: '54.94.190.76:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
-    {nome: 'Revenda', url: '54.94.190.76:8001', servicos : ['security', 'gestao', 'blitz-refugo']}
+    {nome: 'GEO SP', url: '172.19.234.137:8001', servicos : ['blitz-refugo']},
+    {nome: 'GEOs CO, RJ e SUL', url: '172.19.234.48:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
+    {nome: 'GEOs MG, NE e NO', url: '172.19.234.49.:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
+    {nome: 'Guarulhos', url: '172.19.234.67:8001', servicos : ['security', 'gestao', 'blitz-refugo']},
+    {nome: 'Revenda', url: '186.250.185.187.:8001', servicos : ['security', 'gestao', 'blitz-refugo']}
   ]
 };
